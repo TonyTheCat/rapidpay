f7ad3cfd0c91c213599c43978f11418645c37bf1
tonythecat <anton.shishenya@gmail.com>
2017-10-17 19:58:18 +0300

    Continue working on passport

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const http = require('http');
 const passport = require('./common/passport');
 const settings = require('./config').settings;
 const log = require('./common/logger').getLogger('app');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-// Router
+// passport aux methods
 app.use(passport.initialize());
+app.use(passport.session());
+
+// Router
+app.use('/', require('./routes/index'));
