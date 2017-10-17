9176666b7d469a4b969fc4b8fa84727a26847077
tonythecat <anton.shishenya@gmail.com>
2017-10-17 19:21:24 +0300

    Add mongoose and basic methods for passport

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const http = require('http');
+const passport = require('./common/passport');
+const settings = require('./config').settings;
+const log = require('./common/logger').getLogger('app');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// Router
+app.use(passport.initialize());
